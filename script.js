7f19c3965145dc1d1fd21bf7a9e4fc5cf79b26de
joan30122001 <ngounouloic675@gmail.com>
2024-08-07 12:31:17 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.querySelectorAll('.card-flip').forEach(card => {
+//       card.addEventListener('click', () => {
+//         card.classList.toggle('flipped');
+//       });
+//     });
+//   });
+  
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.card-flip').forEach(card => {
+      card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+      });
+    });
+  });
+  
